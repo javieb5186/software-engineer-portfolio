c9cb8accdb20f0a4ac5dd5d5c42650e8c105b6a6
javieb5186 <javieb5186@gmail.com>
2023-11-10 13:47:52 -0700

    Add About Me component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <Hero />
+      <AboutMe />
     </div>
   )
 }
